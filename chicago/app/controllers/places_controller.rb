f949a246644856ce23ef3fd239e2d05f0b161f6a
Dana Katz <danakatz@me.com>
2015-04-21 00:24:26 -0500

    Add show and delete methods

--- chicago/app/controllers/places_controller.rb
+++ chicago/app/controllers/places_controller.rb
@@ -4,4 +4,14 @@ class PlacesController < ApplicationController
     @places = Place.all
   end
 
+  def show
+    @place = Place.find_by_id(params[:id])
+  end
+
+  def delete
+    place = Place.find_by_id(params[:id])
+    place.delete
+    redirect_to "/places"
+  end
+
 end
